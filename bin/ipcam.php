71e96d846c0a036f63da8f73daa927235a451b50
Jorge Alberto Cricelli <jacricelli@gmail.com>
2021-01-14 10:27:25 -0300

    Actualizar ejecutable de la aplicación

--- bin/ipcam.php
+++ bin/ipcam.php
@@ -1,11 +1,23 @@
-#!/usr/bin/php -q
 <?php
 declare(strict_types=1);
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-use App\Application;
-use Cake\Console\CommandRunner;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Dotenv\Dotenv;
 
-$runner = new CommandRunner(new Application(), 'tool');
-exit($runner->run($argv));
+$application = new Application('IPCam', '@package_version@');
+
+try {
+    $pharRunning = Phar::running(false);
+    if ($pharRunning) {
+        $pharRunning = dirname($pharRunning);
+    }
+    (new Dotenv())->loadEnv($pharRunning ?: __DIR__ . '/.env');
+} catch (\Exception $e) {
+    echo "ERROR: No se ha encontrado el archivo de configuración o el formato del mismo no es válido.\n";
+    exit(1);
+}
+
+/** @noinspection PhpUnhandledExceptionInspection */
+$application->run();
