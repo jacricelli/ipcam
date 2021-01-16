00781376d418c427189ad492709ee172347b542f
Jorge Alberto Cricelli <jacricelli@gmail.com>
2021-01-16 01:51:29 -0300

    Eliminar método sin utilidad

--- src/IPCam.php
+++ src/IPCam.php
@@ -228,39 +228,6 @@ class IPCam
 //    }
 //
 //    /**
-//     * Obtiene el contenido de la página especificada
-//     *
-//     * @param int $number Número de página
-//     * @return string
-//     * @throws \RuntimeException
-//     */
-//    private function getPage(int $number = 1): string
-//    {
-//        $ch = curl_init();
-//        curl_setopt($ch, CURLOPT_URL, sprintf('%s/rec/rec_file.asp?page=%d', $this->url, $number));
-//        curl_setopt($ch, CURLOPT_USERPWD, $this->credentials);
-//        curl_setopt($ch, CURLOPT_HEADER, 0);
-//        curl_setopt($ch, CURLOPT_BUFFERSIZE, 65536);
-//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-//        curl_setopt($ch, CURLOPT_FAILONERROR, true);
-//        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-//
-//        $error = null;
-//        $content = curl_exec($ch);
-//        if (curl_errno($ch)) {
-//            $error = curl_error($ch);
-//        }
-//        curl_close($ch);
-//
-//        if ($error) {
-//            throw new \RuntimeException($error);
-//        }
-//
-//        return $content;
-//    }
-//
-//    /**
 //     * Elimina una grabación
 //     *
 //     * @param \App\Library\RecordingInterface $recording Grabación
